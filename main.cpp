1ff6d51d909c18dc2f7e0532d0fdcfe8d76b26fc
Ewa Kwiecień <ewa.kwiecien.programista@gmail.com>
2020-07-09 23:28:59 +0200

    written class User

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-
+#include "User.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    User user;
+    user.setId(1);
+    user.setName("Ewa");
+    user.setLastName("Kwiecien");
+    user.setLogin("123ewa");
+    user.setPassword("1220");
+
+    cout << user.getId() << endl;
+    cout << user.getName() << endl;
+    cout << user.getLastName() << endl;
+    cout << user.getLogin() << endl;
+    cout << user.getPassword() << endl;
     return 0;
 }
