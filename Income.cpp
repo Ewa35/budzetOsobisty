a48568890e0b50642127dd26b808789ef571187c
Ewa Kwiecień <ewa.kwiecien.programista@gmail.com>
2020-08-03 03:06:12 +0200

    written class Income

---
+++ Income.cpp
@@ -0,0 +1,43 @@
+#include "Income.h"
+
+void Income::setUserId (int newUserId)
+{
+    userId=newUserId;
+}
+    void Income::setId (int newId)
+    {
+        id=newId;
+    }
+    void Income::setIncomeCategory (string newIncomeCategory)
+    {
+        incomeCategory=newIncomeCategory;
+    }
+    void Income::setDate (int newDate)
+    {
+        date=newDate;
+    }
+    void Income::setValue (int newValue)
+    {
+        value=newValue;
+    }
+
+    int Income::getUserId()
+    {
+        return userId;
+    }
+    int Income::getId()
+    {
+        return id;
+    }
+    string Income::getIncomeCategory()
+    {
+        return incomeCategory;
+    }
+    int Income::getDate()
+    {
+        return date;
+    }
+    double Income::getValue()
+    {
+        return value;
+    }
